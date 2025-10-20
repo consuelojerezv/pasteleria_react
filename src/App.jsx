7ee19322eaeb0ee2e96906ee4524f560c5603720
consuelojerezv <co.jerezv@duocuc.cl>
2025-10-20 20:38:16 -0300

    importacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "/./components/Navbar";
 import Home from "./pages/Home";
 import Productos from "./pages/Productos";
 import Carrito from "./pages/Carrito";
