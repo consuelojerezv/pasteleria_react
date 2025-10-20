bd0e74ddaedc9f464a670e0298276d765796fa1d
ManeFerG <vale.gomezv@duocuc.cl>
2025-10-20 20:07:30 -0300

    cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ export default function Home() {
       <div className="text-center mb-4">
         <h1>🎂 Pastelería Mil Sabores</h1>
         
-        <p className="text-muted">Dulces que alegran el alma</p>
-        <a className="btn btn-dark mt-2" href="/blog">Ver novedades</a>
+        <p className="text-muted">Pastelería "Mil Sabores" Sabores celebra su 50 aniversario como un referente en la repostería chilena. Famosa por su participación en un récord Guinness en 1995, cuando colaboró en la creación de la torta más grande del mundo, la pastelería busca renovar su sistema de ventas online para ofrecer una experiencia de compra moderna y accesible para sus clientes.</p>
+        <a className="btn btn-dark mt-2" href="/blog">Ver Productos</a>
       </div>
     </main>
   );
